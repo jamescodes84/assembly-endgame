77997cea4345f151df930083f84a84a29b48919d
jamescodes84 <james.reynolds9@icloud.com>
2025-06-14 15:08:43 -0600

    header styled

--- src/components/EndGameBanner.jsx
+++ src/components/EndGameBanner.jsx
@@ -1,16 +1,18 @@
 export default function EndGameBanner() {
-    let roundOver = false /* this will eventually come in via props */
+    let roundOver = true /* this will eventually come in via props */
     let playerWonRound = true /*this will eventually come in via props */
     
     return (
         <>
         
-            {roundOver ?
-            <div >
-            <div class={`endgame-banner-${playerWonRound ? "win":"lose"}`}>{playerWonRound ? "YOU WIN!!!": "YOU LOSE!!!"}</div>
-            <div class={`endgame-banner-${playerWonRound ? "win":"lose"}`}>{playerWonRound ? "Well done! 🎉":  "Better start learning Assembly! 🤖"}</div>
-            </div> :
-            <div className="endgame-banner-empty"></div>
+            {
+                roundOver ?
+                    <div >
+                    <div class={`endgame-banner-${playerWonRound ? "win":"lose"}`}>{playerWonRound ? "YOU WIN!!!": "YOU LOSE!!!"}</div>
+                    <div class={`endgame-banner-${playerWonRound ? "win":"lose"}`}>{playerWonRound ? "Well done! 🎉":  "Better start learning Assembly! 🤖"}</div>
+                    </div> 
+                :
+                    <div className="endgame-banner-empty"></div>
             }
             
         </>
